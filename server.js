1d1e067a2fe3f6c8a6e1c6af9daaea6c544e460b
Olukeye <seunolukeye2000@gmail.com>
2022-02-11 14:02:19 +0100

    fixes

--- server.js
+++ server.js
@@ -5,82 +5,82 @@ const {ApolloServer, gql } = require("apollo-server");
 const categories = [
     {
         id:"9h3n6gf-987l-6547-665756656vt89023h4",
-        name:"Gadgets",
+        name:"IT",
     },
     {
         id:"20ate6j-987l-6fa7-gf56h4551vt89023h4",
-        name:"games",
+        name:"Public",
     },
     {
         id:"t564083-er7l-8098-k7475665n40126723h",
-        name:"beauti",
+        name:"Banking"
     },
 ]
-// products
-const products = [
+// Jobs
+const jobs = [
     {
         id:"646gfgf-t65i-7bv7-665756656vt29hf48",
-        name:"seun olukeie",
-        description:"Wifi",
-        quantity: 1,
-        image:"img-1",
-        price:21.767,
-        onSale:false
+        title:"Software Engineer",
+        company:"Tranferano Nig. Resouces",
+        description:"We are looking for dynamic, energetic interns who are eager  to join FlexiSAF where they will be fully immersed into the world of software development.",
+        role: "Junior",
+        amount:500000,
+        location:"Lagos, Nigeria"
     },
     {
         id:"646gfgf-t65i-7bv7-665756656vt656fv7",
-        name:"stanlei",
-        description:"Xbox",
-        quantity: 1,
-        image:"img-2",
-        price:646.767,
-        onSale:false
+        title:"IT Support / Graphics Design Officer",
+        company:"MEG Consults",
+        description:"IT Support Officer ResponsibilitiesMainly responsible for the smooth running of computer systems, routers, switches, internet and ensure users get maximum benefits from them.",
+        role: "Training",
+        amount:50000,
+        location:"Lagos, Nigeria"
     },
     {
         id:"646gfgf-t65i-7bv7-6657-56656vt09gd84",
-        name:"victor",
-        description:"ps 1",
-        quantity: 1,
-        image:"img-3",
-        price:646.767,
-        onSale:false
+        title:"Banking And Finance",
+        company:"gtBank",
+        description:"This job aims to help the employer in personal, professional, and spiritual areas.",
+        role: "Training",
+        amount:75000,
+        location:"Lagos, Nigeria"
     },
     {
         id:"6473gh5-t65i-7bv7-6657-56656vt29hf43",
-        name:"Rose-marri",
-        description:"make up kit",
-        quantity: 5,
-        image:"img-4",
-        price:646.767,
-        onSale:false
+        title:"Mechanical Engineer",
+        company:"Osh-Auto",
+        description:"To oversee all mechanical engineering operation and activities consisting of installation etc",
+        role:"Technician",
+        amount:120000,
+        location:"Lagos, Nigeria"
     },
     {
         id:"646gfgf-t65i-7bv7-66575hd65kt29hf42",
-        name:"Zidane",
-        description:"hp pc",
-        quantity: 1,
-        image:"img-5",
-        price:646.767,
-        onSale:false
+        title:"Civil Service",
+        company:"Federal Government",
+        description:"Lagos satet waste management .",
+        role: "training",
+        amount:65000,
+        location:"Lagos, Nigeria"
     },
 ]
 const typeDefs = gql `
    type Query{
        hello: String,
-       products:[Product!]!         ##get all products
-       product(id:ID!):Product     ##getting a single product bi its id
+       jobs:[Job!]!         ##get all jobs
+       job(id:ID!):Job    ##getting a single job bi its id
        categories:[Category!]!      ##
        category(id:ID!):Category
    }
 
-   type Product {
-       id:ID!
-    name: String!
-    description:String!
-    quantity:Int!
-    image:String!
-    price:Float!
-    onSale:Boolean!
+   type Job {
+        id:ID!
+        title:String!
+        company:String!
+        description:String!
+        role:String!
+        amount:String!
+        location:String!
 },
 
 type Category {
@@ -94,15 +94,15 @@ const resolvers = {
         hello: () => {
             return "Hello GraghQL" 
         },
-        // get all products
-        products: (parant, args, context)=> {
-            return products
+        // get all jobs
+        jobs: (parant, args, context)=> {
+            return jobs
         },
-        // get single product
-        product: (parant, args, context)=> {
+        // get single job
+        job: (parant, args, context)=> {
             const {id} = args;
-            // find product from [ products ] where productID matches searched product
-            return product =  products.find( (product) => product.id === id);
+            // find job from [ jobs ] where jobID matches searched job
+            return job =  jobs.find( (job) => job.id === id);
         },
         // List Categories
         categories: (parant, args, context) => categories,
