e9ce1c9651b3e4672f2e3a017d8b06ddec16d7c4
Olukeye <seunolukeye2000@gmail.com>
2022-03-12 23:43:02 +0100

    add mutation

--- type.schema.js
+++ type.schema.js
@@ -3,11 +3,14 @@ const { gql } = require("apollo-server");
 
 exports.typeDefs = gql `
 type Query{
-    hello: String,
     jobs:[Job!]!         ##get all jobs
     job(id:ID!):Job    ##getting a single job by its id
     categories:[Category!]!      ##get all categories
     category(id:ID!):Category    ## get a single category by id
+}
+
+type Mutation {
+    createCategory(input: createCategoryInput!) : Category!
 }
 
 type Job {
